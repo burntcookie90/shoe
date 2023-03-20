ddcd43dd8b83c0115c16c0549a95046ba0df5d38
Vishnu Rajeevan <github@vishnu.email>
2023-03-20 17:58:47 -0400

    add proxy function generation

--- processor/src/main/kotlin/com/vishnurajeevan/shoe/ShoeProcessor.kt
+++ processor/src/main/kotlin/com/vishnurajeevan/shoe/ShoeProcessor.kt
@@ -1,5 +1,6 @@
 package com.vishnurajeevan.shoe
 
+import androidx.compose.runtime.Composable
 import com.google.auto.service.AutoService
 import com.google.devtools.ksp.KspExperimental
 import com.google.devtools.ksp.processing.CodeGenerator
@@ -9,23 +10,26 @@ import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
 import com.google.devtools.ksp.symbol.KSAnnotated
-import com.google.devtools.ksp.symbol.KSClassDeclaration
 import com.google.devtools.ksp.symbol.KSFunctionDeclaration
 import com.google.devtools.ksp.symbol.KSType
 import com.google.devtools.ksp.symbol.KSVisitorVoid
 import com.google.devtools.ksp.validate
 import com.squareup.kotlinpoet.AnnotationSpec
 import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
 import com.squareup.kotlinpoet.ParameterSpec
 import com.squareup.kotlinpoet.ParameterizedTypeName
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.TypeName
 import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.asClassName
 import com.squareup.kotlinpoet.ksp.toAnnotationSpec
 import com.squareup.kotlinpoet.ksp.toTypeName
 import com.squareup.kotlinpoet.ksp.writeTo
 import com.vishnurajeevan.shoe.annotation.ShoeIn
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class ShoeProcessor(
@@ -75,8 +79,9 @@ class ShoeProcessor(
         .toMap()
       val presenterData = PresenterData(
         packageName = function.packageName.asString(),
+        presenterName = function.simpleName.getShortName(),
         className = "${function.simpleName.getShortName()}DependencyContainer",
-        parameters = parameters,
+        parameters = parameters as LinkedHashMap<String, TypeName>,
         returnType = function.returnType!!.toTypeName(),
         flowType = eventClass
       )
@@ -88,13 +93,38 @@ class ShoeProcessor(
           .addAnnotation(Inject::class)
 
         presenterData.parameters.map { (name, type) ->
-          ParameterSpec(name, type)
+          ParameterSpec.builder(name, type)
+            .build()
         }.forEach(constructorBuilder::addParameter)
 
         val constructor = constructorBuilder.build()
+
+        val statement = buildString {
+          append("return ${presenterData.presenterName}(events = events,")
+          presenterData.parameters.forEach { (name, type) ->
+            append("$name = %$name:L,")
+          }
+          append(")")
+        }
+
+        val params = presenterData.parameters
+          .map { (name, _) -> name to name }
+          .toMap()
+        val codeblock = CodeBlock.builder()
+          .addNamed(statement, params)
+          .build()
+
+        val modelsFunction = FunSpec.builder("models")
+          .addAnnotation(Composable::class)
+          .addParameter(ParameterSpec("events", Flow::class.asClassName().parameterizedBy(presenterData.flowType)))
+          .returns(presenterData.returnType)
+          .addCode(codeblock)
+          .build()
+
         addType(
           TypeSpec.classBuilder(presenterData.className)
             .primaryConstructor(constructor)
+            .addFunction(modelsFunction)
             .build()
         )
       }.build()
@@ -104,10 +134,11 @@ class ShoeProcessor(
 
   data class PresenterData(
     val packageName: String,
+    val presenterName: String,
     val className: String,
-    val parameters: Map<String, TypeName>,
+    val parameters: LinkedHashMap<String, TypeName>,
     val returnType: TypeName,
-    val flowType: TypeName
+    val flowType: TypeName,
   )
 }
 
