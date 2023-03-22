ce7a8efa8bf5b3990c7a1acb88459f17cd0d38ad
Vishnu Rajeevan <github@vishnu.email>
2023-03-21 20:59:56 -0400

    Make sure to exclude the model types in a presenter

--- processor/src/main/kotlin/com/vishnurajeevan/shoe/ShoeProcessor.kt
+++ processor/src/main/kotlin/com/vishnurajeevan/shoe/ShoeProcessor.kt
@@ -19,9 +19,11 @@ import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.CodeBlock
 import com.squareup.kotlinpoet.FileSpec
 import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
 import com.squareup.kotlinpoet.ParameterSpec
 import com.squareup.kotlinpoet.ParameterizedTypeName
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
+import com.squareup.kotlinpoet.PropertySpec
 import com.squareup.kotlinpoet.TypeName
 import com.squareup.kotlinpoet.TypeSpec
 import com.squareup.kotlinpoet.asClassName
@@ -70,11 +72,13 @@ class ShoeProcessor(
       val shoeIn = function.annotations.filter {
         it.shortName.getShortName() == ShoeIn::class.simpleName
       }.first()
+      val returnType = function.returnType!!.toTypeName()
       val eventClass = (shoeIn.arguments.first().value as KSType).toTypeName()
       val parameters = function.parameters
         .map { it.name!!.getShortName() to it.type.toTypeName() }
         .filterNot { (_, type) ->
-          (type is ParameterizedTypeName) && type.typeArguments.first() == eventClass
+          ((type is ParameterizedTypeName) && type.typeArguments.first() == eventClass)
+              || type == returnType
         }
         .toMap()
       val presenterData = PresenterData(
@@ -82,7 +86,7 @@ class ShoeProcessor(
         presenterName = function.simpleName.getShortName(),
         className = "${function.simpleName.getShortName()}DependencyContainer",
         parameters = parameters as LinkedHashMap<String, TypeName>,
-        returnType = function.returnType!!.toTypeName(),
+        returnType = returnType,
         flowType = eventClass
       )
       val fileSpec = FileSpec.builder(
@@ -97,12 +101,19 @@ class ShoeProcessor(
             .build()
         }.forEach(constructorBuilder::addParameter)
 
+        val properties = presenterData.parameters.map { (name, type) ->
+          PropertySpec.builder(name, type)
+            .initializer(name)
+            .addModifiers(KModifier.PRIVATE)
+            .build()
+        }
+
         val constructor = constructorBuilder.build()
 
         val statement = buildString {
-          append("return ${presenterData.presenterName}(events = events,")
+          append("return ${presenterData.presenterName}(\nevents = events,\n")
           presenterData.parameters.forEach { (name, type) ->
-            append("$name = %$name:L,")
+            append("$name = %$name:L,\n")
           }
           append(")")
         }
@@ -124,6 +135,7 @@ class ShoeProcessor(
         addType(
           TypeSpec.classBuilder(presenterData.className)
             .primaryConstructor(constructor)
+            .addProperties(properties)
             .addFunction(modelsFunction)
             .build()
         )
--- processor/src/test/kotlin/com/vishnurajeevan/shoe/ProcessorTest.kt
+++ processor/src/test/kotlin/com/vishnurajeevan/shoe/ProcessorTest.kt
@@ -5,31 +5,62 @@ import com.tschuchort.compiletesting.SourceFile
 import com.tschuchort.compiletesting.symbolProcessorProviders
 import org.jetbrains.kotlin.compiler.plugin.ExperimentalCompilerApi
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 @OptIn(ExperimentalCompilerApi::class)
 class ProcessorTest {
 
+  val dataModifier = SourceFile.kotlin(
+    "DataModifier.kt", """
+      package com.example.test
+
+      interface DataModifier
+    """.trimIndent()
+  )
+
+  val userRepository = SourceFile.kotlin(
+    "UserRepository.kt", """
+      package com.example.test
+
+      interface UserRepository
+      
+    """.trimIndent()
+  )
+
+  val model = SourceFile.kotlin(
+    "SimpleModel.kt", """
+      package com.example.test
+      
+      data class SimpleModel(
+        val name: String
+      )
+    """.trimIndent()
+  )
+
+  val event = SourceFile.kotlin(
+    "SimpleEvent.kt", """
+      package com.example.test
+
+      sealed interface SimpleEvent {
+        data class Save(val name: String): SimpleEvent
+      }
+    """.trimIndent()
+  )
+  val defaultSources = listOf(dataModifier, userRepository, model, event)
+
   @Test
   fun basic() {
-
     val compilation = KotlinCompilation().apply {
-      sources = listOf(
+      sources = defaultSources + listOf(
         SourceFile.kotlin(
           "SimplePresenter.kt", """
-          package com.exmaple.test
+          package com.example.test
+
           import androidx.compose.runtime.Composable
           import com.vishnurajeevan.shoe.annotation.ShoeIn
           import kotlinx.coroutines.flow.Flow
       
-          interface DataModifier
-          interface UserRepository
-      
-          data class SimpleModel(val name: String)
-          sealed interface SimpleEvent {
-            data class Save(val name: String): SimpleEvent
-          }
-      
-          @ShoeIn(eventClass = SimpleEvent::class)
+          @ShoeIn(SimpleEvent::class)
           @Composable
           fun SimplePresenter(
               events: Flow<SimpleEvent>,
@@ -47,29 +78,20 @@ class ProcessorTest {
 
     val result = compilation.compile()
 
-    println(result.exitCode)
+    assertEquals(result.exitCode, KotlinCompilation.ExitCode.OK)
   }
 
   @Test
   fun basic_noComposable() {
 
     val compilation = KotlinCompilation().apply {
-      sources = listOf(
+      sources = defaultSources + listOf(
         SourceFile.kotlin(
           "SimplePresenter.kt", """
-          package test
+          package com.example.test
       
           import com.vishnurajeevan.shoe.annotation.ShoeIn
           import kotlinx.coroutines.flow.Flow
-      
-          interface DataModifier
-          interface UserRepository
-      
-          data class SimpleModel(val name: String)
-      
-          sealed interface SimpleEvent {
-            data class Save(val name: String): SimpleEvent
-          }
       
           @ShoeIn(SimpleEvent::class)
           fun SimplePresenter(
@@ -88,6 +110,41 @@ class ProcessorTest {
 
     val result = compilation.compile()
 
-    println(result.exitCode)
+    assertEquals(result.exitCode, KotlinCompilation.ExitCode.COMPILATION_ERROR)
+    assertEquals(result.messages.contains("@ShoeIn can only be applied to a composable presenter"), true)
+  }
+
+  @Test
+  fun modelParameter() {
+    val compilation = KotlinCompilation().apply {
+      sources = defaultSources + listOf(
+        SourceFile.kotlin(
+          "SimplePresenter.kt", """
+          package com.example.test
+
+          import androidx.compose.runtime.Composable
+          import com.vishnurajeevan.shoe.annotation.ShoeIn
+          import kotlinx.coroutines.flow.Flow
+      
+          @ShoeIn(SimpleEvent::class)
+          @Composable
+          fun SimplePresenter(
+              initialModel: SimpleModel = SimpleModel("meow"),
+              events: Flow<SimpleEvent>,
+              modifier: DataModifier,
+              repo: UserRepository
+          ): SimpleModel {
+            return SimpleModel("john")
+          }
+        """.trimIndent()
+        )
+      )
+      symbolProcessorProviders = listOf(ShoeProcessorProvider())
+      inheritClassPath = true
+    }
+
+    val result = compilation.compile()
+
+    assertEquals(result.exitCode, KotlinCompilation.ExitCode.OK)
   }
 }
