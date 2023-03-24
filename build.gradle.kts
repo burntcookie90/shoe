fccc845e9115765a0db8e486d2a17a9318446ac6
Vishnu Rajeevan <github@vishnu.email>
2023-03-23 21:57:30 -0400

    move pom info to gradle.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,37 +20,6 @@ allprojects {
   repositories {
     mavenCentral()
   }
-
-  plugins.withId("com.vanniktech.maven.publish.base") {
-    configure<MavenPublishBaseExtension> {
-
-      pom {
-        name.set("shoe")
-        description.set("A ksp processor to shoe in your composable presenters.")
-        inceptionYear.set("2023")
-        url.set("https://github.com/burntcookie90/shoe")
-        licenses {
-          license {
-            name.set("The Apache License, Version 2.0")
-            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-            distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-          }
-        }
-        developers {
-          developer {
-            id.set("burntcookie90")
-            name.set("Vishnu Rajeevan")
-            url.set("https://github.com/burntcookie90")
-          }
-        }
-        scm {
-          url.set("https://github.com/burntcookie90/shoe")
-          connection.set("scm:git:git://github.com/burntcookie90/shoe.git")
-          developerConnection.set("scm:git:ssh://git@github.com/burntcookie90/shoe.git")
-        }
-      }
-    }
-  }
 }
 
 mavenPublishing {
