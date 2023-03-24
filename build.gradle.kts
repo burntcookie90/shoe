9f35427780b848f7e4d73dfba84fe156d0a95b71
Vishnu Rajeevan <github@vishnu.email>
2023-03-23 22:11:08 -0400

    dont apply maven publish to root

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import com.vanniktech.maven.publish.SonatypeHost
 plugins {
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.ksp) apply false
-  alias(libs.plugins.maven.publish)
+  alias(libs.plugins.maven.publish) apply false
 }
 
 buildscript {
